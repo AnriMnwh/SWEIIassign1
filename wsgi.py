197932833a67dc7e13c512f5dc226a802d4f71d1
Anari Manwah <71801134+AnriMnwh@users.noreply.github.com>
2024-09-29 19:30:46 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -67,3 +67,62 @@ def user_tests_command(type):
     
 
 app.cli.add_command(test)
+
+from App.controllers import model_control #my code starts here as I tried to figure out how to write it properly and didnt want to break it
+
+course_cli = AppGroup('course', help='Course object commands')
+
+@course_cli.command("create", help="Creates a course")
+@click.argument("course_name")
+def createcourse(course_name):
+    model_control.createcourse(course_name)
+    print(f'Course {course_name} created!')
+
+@course_cli.command("assign", help="Assigns staff to a course")
+@click.argument("course_id")
+@click.argument("staff_id")
+def assignstaff(course_id, staff_id):
+    model_control.assignstaff(course_id, staff_id)
+    print(f'Staff with ID {staff_id} assigned to course with ID {course_id}!')
+
+@course_cli.command("view_staff", help="View staff assigned to a course")
+@click.argument("course_id")
+def viewstaff(course_id):
+    staff = model_control.getstaff(course_id)
+    print(f'Staff for course with ID {course_id}: {staff}')
+
+@course_cli.command("view", help="View a course")
+@click.argument("course_id")
+def viewcourse(course_id):
+    course = model_control.viewcourse(course_id)
+    if course:
+        print(f'Course ID: {course["id"]}, Course Name: {course["name"]}')
+        for staff in course['staff']:
+            print(f'Staff Name: {staff[0]}, Role: {staff[1]}')
+    else:
+        print(f'Course with ID {course_id} not found.')
+
+app.cli.add_command(course_cli)
+
+staff_cli = AppGroup('staff', help='Staff object commands')
+
+@staff_cli.command("create_lecturer", help="Creates a lecturer")
+@click.argument("lecturer_name")
+def createlecturer(lecturer_name):
+    model_control.createlecturer(lecturer_name)
+    print(f'Lecturer {lecturer_name} created!')
+
+@staff_cli.command("create_ta", help="Creates a TA")
+@click.argument("ta_name")
+def createta(ta_name):
+    model_control.createta(ta_name)
+    print(f'TA {ta_name} created!')
+
+@staff_cli.command("create_tutor", help="Creates a tutor")
+@click.argument("tutor_name")
+def createtutor(tutor_name):
+    model_control.createtutor(tutor_name)
+    print(f'Tutor {tutor_name} created!')
+
+app.cli.add_command(staff_cli)
+
